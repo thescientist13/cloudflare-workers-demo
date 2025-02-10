c9b0d91f9a093f7803726050f3abcd093265f813
Owen Buckley <owenbuckley13@gmail.com>
2025-02-10 14:32:16 -0500

    process.argv work around test

--- functions/api/new-url.js
+++ functions/api/new-url.js
@@ -1,4 +1,9 @@
-const dataUrl = new URL('../public/data.json', import.meta.url);
+import path from 'path';
+
+const fakeImportMetaUrl = `file://${path.dirname(process.argv[1])}/`;
+const dataUrl = new URL('../../public/data.json', fakeImportMetaUrl);
+
+console.log({ fakeImportMetaUrl, dataUrl });
 
 export function onRequest(context) {
   return new Response(dataUrl.href);
